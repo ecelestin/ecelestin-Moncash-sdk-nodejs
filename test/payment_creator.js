5038f6a2979e7032924a03c0e82ba536dc3ea018
Esdras SUY <esdras.suy@hopitalweb.ht>
2019-05-28 13:18:56 -0400

    Added redirect uri after create payment;

--- test/payment_creator.js
+++ test/payment_creator.js
@@ -9,12 +9,13 @@ var create_payment_json = {
 };
 
 
-moncash.payment.create(create_payment_json, function (error, payment) {
+var payment_creator = moncash.payment;
+payment_creator.create(create_payment_json, function (error, payment) {
     if (error) {
         console.log(error);
         throw error;
     } else {
         console.log("Create Payment Response");
-        console.log(payment);
+        console.log(payment_creator.redirect_uri(payment));
     }
 });
