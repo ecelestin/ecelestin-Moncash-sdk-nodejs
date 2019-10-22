832386ab46534a8f9bb0f9db30a23064c19bcb13
Esdras SUY <suyeasdras@gmail.com>
2019-10-22 23:14:24 +0200

    Fixed some warning

--- test/configure.js
+++ test/configure.js
@@ -5,3 +5,9 @@ moncash.configure({
     'client_id': 'c1bf0a27d6bbb217a599c9e25480c11d',
     'client_secret': 'oHrr4tbnB1PH0uz6VQNUvVVDNVNvk0WiIXZWBAed4-CBCwilT8yUdS87AZoPrtqN'
 });
+
+// moncash.configure({
+//     'mode': 'live', //sandbox or live
+//     'client_id': 'c1bf0a27d6bbb217a599c9e25480c11d',
+//     'client_secret': 'gbUxth9_w7-XtNOIHcQ-YAPxE-ez8MVC1r1XfYcDviTK-l68Z8hkwO4JepS_nSjy'
+// });
